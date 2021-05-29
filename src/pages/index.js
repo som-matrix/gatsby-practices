b146858cf118275362e3782fb517f1691810e0c5
SATYA SWAROOP MOHAPATRA <satya@codemancers.com>
2021-05-29 22:42:49 +0530

    feat: pulling data using GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
-const HomePage = () => {
+import { graphql } from "gatsby";
+const HomePage = ({ data }) => {
   return (
     <Layout pageTitle="Homepage">
       <p>This is home page</p>
@@ -9,7 +10,17 @@ const HomePage = () => {
         src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
         alt="mountains"
       />
+      <p>{data.site.siteMetadata.title} </p>
     </Layout>
   );
 };
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
 export default HomePage;
